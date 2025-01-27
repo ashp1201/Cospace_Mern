86a1d66afdea8464f6a26472192ba9718abd84f0
Ashok Purohit <113554853+ashp1201@users.noreply.github.com>
2025-01-28 02:52:36 +0530

    Delete server directory

--- server/Model/roles.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export const RolesSchema = new mongoose.Schema({
-    role:String,
-    permissions:[{type:String}]
-})
-
-export default mongoose.model.Roles || mongoose.model('Role',RolesSchema);
