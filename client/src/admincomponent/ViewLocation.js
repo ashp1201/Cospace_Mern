f62375874be99b847305e8c8219171e8050805b0
Ashok Purohit <113554853+ashp1201@users.noreply.github.com>
2025-01-28 21:25:02 +0530

    Update ViewLocation.js

--- client/src/admincomponent/ViewLocation.js
+++ client/src/admincomponent/ViewLocation.js
@@ -1,103 +1,97 @@
 import React, { useState, useEffect } from 'react';
-import img1 from '../asserts/off10.png'
 import './ViewLocation.css';
 import Cards from './Cards';
 import { getlocation } from '../helper/helper';
 
 function ViewLocation() {
-  // Create state variables for different user types
-  const [freeLocations, setFreeLocations] = useState([]);
-  const [basicLocations, setBasicLocations] = useState([]);
-  const [advanceLocations, setAdvanceLocations] = useState([]);
+  // State to hold locations based on user types
+  const [locations, setLocations] = useState({
+    free: [],
+    basic: [],
+    advance: [],
+  });
 
+  // Fetch locations for all user types
   useEffect(() => {
-    // Fetch locations for each user type and update the respective state variable
-    getlocation({ user_type: 'free' })
-      .then(data => {
-        setFreeLocations(data);
-      })
-      .catch(error => {
-        console.error('Error fetching free data:', error);
-      });
+    const userTypes = ['free', 'basic', 'advance'];
 
-    getlocation({ user_type: 'basic' })
-      .then(data => {
-        setBasicLocations(data);
-      })
-      .catch(error => {
-        console.error('Error fetching basic data:', error);
-      });
+    const fetchLocations = async () => {
+      try {
+        for (const type of userTypes) {
+          const data = await getlocation({ user_type: type });
+          setLocations((prev) => ({
+            ...prev,
+            [type]: data,
+          }));
+        }
+      } catch (error) {
+        console.error('Error fetching locations:', error);
+      }
+    };
 
-    getlocation({ user_type: 'advance' })
-      .then(data => {
-        setAdvanceLocations(data);
-      })
-      .catch(error => {
-        console.error('Error fetching advance data:', error);
-      });
+    fetchLocations();
   }, []);
 
+  // Function to update the location in the parent state
+  const handleUpdateLocation = (updatedLocation) => {
+    setLocations((prevLocations) => {
+      const updatedLocations = { ...prevLocations };
+      // Update the correct user type's locations
+      const userType = updatedLocation.userType; // Assuming userType is passed in the updated location data
+      updatedLocations[userType] = updatedLocations[userType].map((location) =>
+        location._id === updatedLocation._id ? updatedLocation : location
+      );
+      return updatedLocations;
+    });
+  };
+
+  // Component to render locations dynamically
+  const RenderLocations = ({ userType, locations }) => (
+    <div>
+      <h2 className="viewlocation_header">
+        For {userType.charAt(0).toUpperCase() + userType.slice(1)} Users
+      </h2>
+      <div className={`cards_${userType}`}>
+        {locations.length > 0 ? (
+          locations.map((location) => (
+            <Cards
+              key={location._id} // Ensure the unique key is correct
+              id={location._id}
+              nameofplace={location.name_location}
+              space={location.space_location}
+              img={location.photo}
+              contact={location.owner_contact}
+              description={location.description}
+              price={location.amount}
+              userType={userType}
+              latitude={location.latitude}
+              longitude={location.longitude}
+              onDelete={(id) => {
+                // Remove the location with the specific _id from the locations list
+                setLocations((prevLocations) => ({
+                  ...prevLocations,
+                  [userType]: prevLocations[userType].filter((loc) => loc._id !== id),
+                }));
+              }}
+              onUpdate={handleUpdateLocation} // Pass onUpdate function to update location
+            />
+          ))
+        ) : (
+          <p>No locations available for {userType} users.</p>
+        )}
+      </div>
+    </div>
+  );
+
   return (
-    <div className='viewlocation'>
-      <div>
-        <h2 className='viewlocation_header'>For Free Users</h2>
-        <div className='cards_free'>
-          {freeLocations.map(location => (
-            <Cards
-              key={location.id}
-              nameofplace={location.name_location}
-              space={location.space_location}
-              img={location.photo}
-              contact={location.owner_contact}
-              description={location.description}
-              price={location.amount}
-              userType='free'
-              latitude={location.latitude}
-              longitude={location.longitude}
-            />
-          ))}
-        </div>
-      </div>
-
-      <div>
-        <h2 className='viewlocation_header'>For Basic Users</h2>
-        <div className='cards_basic'>
-          {basicLocations.map(location => (
-            <Cards
-              key={location.id}
-              nameofplace={location.name_location}
-              space={location.space_location}
-              img={location.photo}
-              contact={location.owner_contact}
-              description={location.description}
-              price={location.amount}
-              userType='basic'
-              latitude={location.latitude}
-              longitude={location.longitude}
-            />
-          ))}
-        </div>
-      </div>
-
-      <div>
-        <h2 className='viewlocation_header'>For Advance Users</h2>
-        <div className='cards_advance'>
-          {advanceLocations.map(location => (
-            <Cards
-              key={location.id}
-              nameofplace={location.name_location}
-              space={location.space_location}
-              img={location.photo}
-              contact={location.owner_contact}
-              description={location.description}
-              price={location.amount}
-              userType='advance'
-              latitude={location.latitude}
-              longitude={location.longitude}
-            />
-          ))}
-        </div>
-      </div>
+    <div className="viewlocation">
+      {['free', 'basic', 'advance'].map((userType) => (
+        <RenderLocations
+          key={userType}
+          userType={userType}
+          locations={locations[userType]}
+        />
+      ))}
     </div>
   );
 }
