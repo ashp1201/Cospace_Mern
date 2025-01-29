1dfbeb8e417420eae9e6db0d68560c47e1226c56
Ashok Purohit <113554853+ashp1201@users.noreply.github.com>
2025-01-30 04:41:15 +0530

    Update ProtectedRoute.js

--- client/src/middleware/ProtectedRoute.js
+++ client/src/middleware/ProtectedRoute.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 const isAuthenticated = () => {
   const cookies = document.cookie.split(';');
   for (const cookie of cookies) {
     const [name, value] = cookie.trim().split('=');
     if (name === 'tokenAdmin') {
-      return true;
+      try {
+        const decodedToken = jwtDecode(value);
+        const currentTime = Date.now() / 1000; // Current time in seconds
+        // Check if token has expired
+        if (decodedToken.exp > currentTime) {
+          return true;
+        } else {
+          return false; // Token has expired
+        }
+      } catch (error) {
+        console.error("Invalid token:", error);
+        return false; // Invalid token format
+      }
     }
   }
   return false;
